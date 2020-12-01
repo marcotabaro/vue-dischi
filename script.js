7ec95c02197081fcf34409188312fc18d3e9eae7
marco <g.marcotaba@gmail.com>
2020-12-02 00:29:44 +0100

    bonus implementation, missing logic

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const myApp = new Vue ({
     el: '#root',
     data: {
         albums: [],
-        genre: 'all'
+        selected: 'all'
     },
     mounted: function() {
             axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
-            .then(albums => { this.albums = albums.data.response})
-    }
-})
+            .then(result => { this.albums = result.data.response})
+        }
+    })
 
 
 
