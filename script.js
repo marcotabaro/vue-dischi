2cb0d0f1505b70716f52eb6144c739c35ee55af7
marco <g.marcotaba@gmail.com>
2020-12-01 18:00:25 +0100

    ajax call to API boolean music

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const myApp = new Vue ({
     el: '#root',
     data: {
-        
+        albums: [],
+        genre: 'all'
+    },
+    mounted: function() {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(albums => { this.albums = albums.data.response})
     }
 })
+
+
+
