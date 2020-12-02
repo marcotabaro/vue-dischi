b84266d1e392685386f8c62bcfd311cbf05cd1bf
marco <g.marcotaba@gmail.com>
2020-12-02 17:24:24 +0100

    bonus completed

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const myApp = new Vue ({
     el: '#root',
     data: {
         albums: [],
+        genres: ['pop', 'rock', 'metal', 'jazz'],
         selected: 'all'
     },
     mounted: function() {
             axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(result => { this.albums = result.data.response})
+        },
+    computed: {
+        filteredAlbums() {
+            if(this.selected === 'all'){
+                return this.albums;
+            } else {
+                return this.albums.filter(el => el.genre.toLowerCase() === this.selected)
+            }
         }
+    }
     })
 
 
